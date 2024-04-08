10455de72481e9381d1b76238aec2115e8a48337
zhangjunbo02 <zhangjunbo02@meituan.com>
2024-04-08 11:20:10 +0800

    test

---
+++ src/app/zjb_typerefl.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author zhangjunbo02
+%%% @copyright (C) 2024, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 08. 4月 2024 11:00
+%%%-------------------------------------------------------------------
+-module(zjb_typerefl).
+-author("zhangjunbo02").
+
+-include_lib("typerefl/include/types.hrl").
+
+%% API
+-export([]).
